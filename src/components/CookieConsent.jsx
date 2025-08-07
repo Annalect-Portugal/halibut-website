e9ef3fadfeba876d1e5e1ff982e20e7f56e277dc
David Lopes <david.lopes@annalect.com>
2025-08-07 17:37:17 +0100

    Update CSP to allow 'unsafe-inline' scripts
    Added 'unsafe-inline' to the script-src directive in vercel.json to support inline scripts, likely required for Google Tag Manager. Also removed unnecessary script attributes in CookieConsent.jsx to align with this change.

--- src/components/CookieConsent.jsx
+++ src/components/CookieConsent.jsx
@@ -12,8 +12,6 @@ const loadGTM = () => {
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-KPL3PGF');`;
-  script.async = true;
-  script.type = 'text/javascript';
   document.head.appendChild(script);
 
   const noscript = document.createElement('noscript');
